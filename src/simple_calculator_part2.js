24fb3fc32941444597b1e99182977cc379fd0741
Lentswe215 <ephraimmamonong@gmail.com>
2020-02-21 10:11:16 +0200

    final commit

--- src/simple_calculator_part2.js
+++ src/simple_calculator_part2.js
@@ -1,54 +1,47 @@
-"use strict"
-
+"use strict";
 class Calculator {
-    constructor(){
-        this.resultArray = []
-        this.memory_slot = []
-        
+  constructor() {
+    this.resultArray = [];
+    this.memory_slot = [];
+  }
+  add() {
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+      if(arguments[i] == "LAST"){
+        sum += this.last()
+      } else if(typeof arguments[i] === "string" && arguments[i].includes("SLOT_")) {
+        let numSlot = arguments[i].match(/\d+/g)
+        Number(numSlot)
+        sum += this.get_slot(numSlot)
+      } else
+      sum += arguments[i];
     }
-     
-    add() {
-        let sum = 0;
-        for(let i = 0; i < arguments.length; i++){
-            if(arguments[i] == "LAST") {
-                sum += this.last()
-            } else if (typeof arguments[i] === "string" && arguments[i].includes("SLOT_")){
-                let slotNumber = arguments[i].match(/\d+/g)
-                Number(slotNumber)
-                sum += this.get_slot(slotNumber)
-            } else
-            sum += arguments[i]
-        }
-        this.resultArray.push(sum)
-        return sum 
+    this.resultArray.push(sum);
+    return sum;
+  }
+  multiply() {
+    let product = 1;
+    for (let i = 0; i < arguments.length; i++) {
+      if(arguments[i] == "LAST"){
+        product *= this.last()
+      } else if(typeof arguments[i] === "string" && arguments[i].includes("SLOT_")) {
+        let numSlot = arguments[i].match(/\d+/g)
+        Number(numSlot)
+        product *= this.get_slot(numSlot)
+      } else
+      product *= arguments[i];
     }
-    multiply() {
-        let product = 1;
-        for( let i = 0; i < arguments.length; i++){
-            if(arguments[i] == "LAST") {
-                product *= this.last()
-            }  else if(typeof arguments[i] === "string" && arguments[i].includes("SLOT_")){
-                let slotNumber = arguments[i].match(/\d+/g)
-                Number(slotNumber)
-                product *= this.get_slot(slotNumber)
-            } else
-            product *= arguments[i]
-        } 
-        this.resultArray.push(product)
-        return product
-    }
-    last() {
-        return this.resultArray[this.resultArray.length - 1]
-    }   
-    
-    set_slot(slotNumber) {
-      this.memory_slot.splice(slotNumber - 1, 0,this.resultArray[slotNumber - 1])
-    }
-
-    get_slot(slotNumber) {
-        return this.memory_slot[slotNumber - 1]
-    }
+    this.resultArray.push(product);
+    return product;
+  }
+  last() {
+    return this.resultArray[this.resultArray.length - 1];
+  }
+  set_slot(numSlot) {
+   this.memory_slot.push(this.resultArray[numSlot - 1])
+  }
+  get_slot(numSlot) {
+   return this.memory_slot[numSlot - 1]
+  }
 }
-
-
 module.exports = Calculator;
